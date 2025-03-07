a08b15ed4e6d6d83dc8360fb05c4b5325c6192d7
LauraBonillaa <158177899+LauraBonillaa@users.noreply.github.com>
2025-03-06 22:28:14 -0500

    commit final

--- Display/index.js
+++ Display/index.js
@@ -5,12 +5,6 @@ document.getElementById("update-btn").addEventListener("click", () => {
         document.getElementById("players").innerText = "Jugadores: " + data.users.join(", ");
         document.getElementById("scores").innerText = "Puntajes: " + JSON.stringify(data.scores);
 
-        setTimeout(() => {
-            users = [];
-            scores = {};
-            choices = {};
-            console.log("Todo reiniciado después de 5 segundos");
-          }, 5000);
     })
     .catch(error => console.error("Error:", error));
 });
